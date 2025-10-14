4b1d3ac68c9264f8c124a075ae1f9a667f7704f5
ngnlananh <lanh111106@gmail.com>
2025-10-15 00:11:18 +0700

    redesign tab bar for staffwindow

--- 1_Development/CoffeeShop/View/Staff/StaffWindow.xaml.cs
+++ 1_Development/CoffeeShop/View/Staff/StaffWindow.xaml.cs
@@ -19,26 +19,6 @@ namespace CoffeeShop.View.Staff
     /// Interaction logic for StaffWindow.xaml
     /// </summary>
     /// 
-    public class GridLengthAnimation : AnimationTimeline
-    {
-        public override Type TargetPropertyType => typeof(GridLength);
-
-        public GridLength From { get; set; }
-        public GridLength To { get; set; }
-
-        public override object GetCurrentValue(object defaultOriginValue, object defaultDestinationValue, AnimationClock animationClock)
-        {
-            double from = From.Value;
-            double to = To.Value;
-
-            if (from > to)
-                return new GridLength((1 - animationClock.CurrentProgress.Value) * (from - to) + to, GridUnitType.Pixel);
-            else
-                return new GridLength(animationClock.CurrentProgress.Value * (to - from) + from, GridUnitType.Pixel);
-        }
-
-        protected override Freezable CreateInstanceCore() => new GridLengthAnimation();
-    }
     public partial class StaffWindow : Window
     {
         private bool isExpanded = false;
@@ -72,24 +52,7 @@ namespace CoffeeShop.View.Staff
 
         private void bdrStaffWindowFunction_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            double from = SidebarColumn.ActualWidth;
-            double to = isExpanded ? 80 : 200;
-
-            var anim = new DoubleAnimation(from, to, TimeSpan.FromMilliseconds(300))
-            {
-                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
-            };
-
-            var gridLengthAnim = new GridLengthAnimation
-            {
-                From = new GridLength(from),
-                To = new GridLength(to),
-                Duration = anim.Duration
-            };
-
-            SidebarColumn.BeginAnimation(ColumnDefinition.WidthProperty, gridLengthAnim);
-
-            isExpanded = !isExpanded;
+            
         }
     }
 }
