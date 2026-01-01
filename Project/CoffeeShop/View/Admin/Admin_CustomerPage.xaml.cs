4d4d2f8f14d5f8fabe317362d18fefb7632aa5d3
Nguyen Nguyen <nathannguyen6002@gmail.com>
2026-01-02 02:03:38 +0700

    feat: staff/customer-page

---
+++ Project/CoffeeShop/View/Admin/Admin_CustomerPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CoffeeShop.ViewModels.AdminVM;
+using System.Windows.Controls;
+
+namespace CoffeeShop.View.Admin
+{
+    /// <summary>
+    /// Interaction logic for Admin_CustomerPage.xaml
+    /// </summary>
+    public partial class AdminCustomerManagementPage : Page
+    {
+        public AdminCustomerManagementPage()
+        {
+            InitializeComponent();
+            this.DataContext = new AdminCustomerViewModel();
+        }
+    }
+}
