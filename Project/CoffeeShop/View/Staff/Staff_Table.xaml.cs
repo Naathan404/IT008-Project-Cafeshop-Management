5d1cd3765d6ae0d28b86e56ef495d9bc00635920
ngnlananh <lanh111106@gmail.com>
2026-01-10 23:30:41 +0700

    feat: Staff_Table page

--- Project/CoffeeShop/View/Staff/Staff_Table.xaml.cs
+++ Project/CoffeeShop/View/Staff/Staff_Table.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CoffeeShop.ViewModels.StaffVM;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CoffeeShop.View.Staff
 {
@@ -20,9 +11,61 @@ namespace CoffeeShop.View.Staff
     /// </summary>
     public partial class Staff_Table : Page
     {
+        StaffTableViewModel _viewModel;
         public Staff_Table()
         {
             InitializeComponent();
+            _viewModel = new StaffTableViewModel();
+            this.DataContext = _viewModel;
+        }
+    }
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Nếu value là null thì hiện (Visible), ngược lại thì ẩn (Collapsed)
+            // Dùng cho dòng "Vui lòng chọn bàn..."
+            return value == null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class NotNullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Nếu có dữ liệu thì hiện, null thì ẩn
+            // Dùng cho bảng chi tiết bàn
+            return value != null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class IntToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int intValue && parameter != null)
+            {
+                // Lấy giá trị so sánh từ ConverterParameter trong XAML
+                int targetValue = int.Parse(parameter.ToString());
+
+                // Nếu status trùng với parameter thì hiện, ngược lại thì ẩn
+                return intValue == targetValue ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
